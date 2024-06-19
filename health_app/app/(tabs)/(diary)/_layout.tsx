b767ec1b665d1cb615396b623a0ae12e2773febe
beovan <truongvangiap04@gmail.com>
2024-06-19 20:36:34 +0700

    update back button

--- health_app/app/(tabs)/(diary)/_layout.tsx
+++ health_app/app/(tabs)/(diary)/_layout.tsx
@@ -9,9 +9,9 @@ import * as SplashScreen from 'expo-splash-screen';
 import {useEffect} from 'react';
 import 'react-native-reanimated';
 import {Link} from 'expo-router';
-
+import { Text } from 'react-native';
 import React from 'react';
-
+import { Ionicons } from '@expo/vector-icons';
 import {useColorScheme} from '@/hooks/useColorScheme';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
@@ -21,19 +21,53 @@ export default function DiaryLayout() {
     return (
         <Stack>
             <Stack.Screen name="step-counter"
-                          options={{headerTitle: 'Bước chân'}}
+                          options={{headerTitle: 'Bước chân',
+                              headerLeft: () => (
+                                  <Link href="home">
+                                      <Text>
+                                          <Ionicons name="arrow-back-sharp" size={24} color="black" />
+
+                                      </Text>
+                                  </Link>
+                              )
+                          }}
             >
             </Stack.Screen>
             <Stack.Screen name="doctor"
-                          options={{headerTitle: 'Bác sĩ'}}
+                          options={{headerTitle: 'Bác sĩ',
+                              headerLeft: () => (
+                                  <Link href="home">
+                                      <Text>
+
+                                          <Ionicons name="arrow-back-sharp" size={24} color="black" />
+                                      </Text>
+                                  </Link>
+                              )
+                          }}
             >
             </Stack.Screen>
             <Stack.Screen name="weight-bmi"
-                          options={{headerTitle: 'Cân nặng & BMI'}}
+                          options={{headerTitle: 'Cân nặng & BMI',
+                              headerLeft: () => (
+                                  <Link href="home">
+                                      <Text>
+                                          <Ionicons name="arrow-back-sharp" size={24} color="black" />
+                                      </Text>
+                                  </Link>
+                              )
+                          }}
             >
             </Stack.Screen>
             <Stack.Screen name="water-reminder"
-                          options={{headerTitle: 'Nhắc uống nước'}}
+                          options={{headerTitle: 'Nhắc uống nước',
+                              headerLeft: () => (
+                                  <Link href="home">
+                                      <Text>
+                                          <Ionicons name="arrow-back-sharp" size={24} color="black" />
+                                      </Text>
+                                  </Link>
+                              )
+                          }}
             >
             </Stack.Screen>
         </Stack>
