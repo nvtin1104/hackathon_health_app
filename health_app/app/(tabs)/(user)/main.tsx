5cf2ad00a8de546eba0b016fcfb6f3375b66df65
beovan <truongvangiap04@gmail.com>
2024-06-26 00:11:18 +0700

    update notification successfully

--- health_app/app/(tabs)/(user)/main.tsx
+++ health_app/app/(tabs)/(user)/main.tsx
@@ -2,8 +2,7 @@ import { ThemedView } from '@/components/ThemedView';
 import { Iconic } from '@/components/icon/Iconic';
 import { Entypo } from '@expo/vector-icons';
 import { router } from 'expo-router';
-import { useState } from 'react';
-import {
+import { useState, useEffect, useCallback } from "react";import {
 	StyleSheet,
 	Image,
 	Alert,
@@ -12,17 +11,22 @@ import {
 	TouchableOpacity,
 	FlatList,
 	VirtualizedList,
+	Switch,
+	Platform
 } from 'react-native';
 import { FontAwesome } from '@expo/vector-icons';
 import { useSession } from '@/auth/ctx';
 import { useStorageState } from '@/auth/useStorageState';
 import { showToast } from '@/utils/toast';
+import * as Device from "expo-device";
+import * as Notifications from "expo-notifications";
 
 type ItemData = {
 	id: string;
 	title: string;
 	icon: any;
 	hidden?: boolean;
+	toggle?: boolean;
 };
 const DATA: ItemData[] = [
 	{
@@ -36,11 +40,18 @@ const DATA: ItemData[] = [
 		icon: 'person-outline',
 	},
 	{
+		id: 'notification',
+		title: 'Thông báo',
+		icon: 'notifications-outline',
+		toggle: true, // Add this line
+	},
+	{
 		id: 'bd7acbea-c1da-46c2-aed5-3ad53abb28ba',
 		title: 'Cài đặt',
 		icon: 'settings-outline',
 	},
 ];
+
 const DATA2: ItemData[] = [
 	{
 		id: 'bd7acbea-c1b1-46c2-aed5-3ad53a4b28ba',
@@ -69,20 +80,120 @@ type ItemProps = {
 	item: ItemData;
 	onPress: () => void;
 };
-const Item = ({ item, onPress }: ItemProps) => (
+Notifications.setNotificationHandler({
+	handleNotification: async () => ({
+		shouldShowAlert: true,
+		shouldPlaySound: true,
+		shouldSetBadge: false,
+	}),
+});
+const Item = ({ item, onPress, isToggleSwitchOn, setIsToggleSwitchOn }: ItemProps) => (
 	<TouchableOpacity onPress={onPress} style={[styles.item]}>
 		<View style={styles.itemLeft}>
 			<Iconic name={item.icon} style={styles.icon} />
 			<Text style={[styles.label]}>{item.title}</Text>
 		</View>
+		{item.toggle && (
+			<Switch
+				value={isToggleSwitchOn}
+				onValueChange={(value) => setIsToggleSwitchOn(value)}
+			/>
+		)}
 		{item.hidden !== true && (
 			<FontAwesome name="chevron-right" size={16} color="black" />
 		)}
 	</TouchableOpacity>
 );
+async function registerForPushNotificationsAsync() {
+	let token;
+
+	if (Platform.OS === "android") {
+		await Notifications.setNotificationChannelAsync("default", {
+			name: "default",
+			importance: Notifications.AndroidImportance.MAX,
+			vibrationPattern: [0, 250, 250, 250],
+			lightColor: "#FF231F7C",
+		});
+	}
+
+	if (Device.isDevice) {
+		const { status: existingStatus } = await Notifications.getPermissionsAsync();
+		let finalStatus = existingStatus;
+		if (existingStatus !== "granted") {
+			const { status } = await Notifications.requestPermissionsAsync();
+			finalStatus = status;
+		}
+		if (finalStatus !== "granted") {
+			if (Device.brand !== "Apple" && Device.brand !== "Microsoft") { // Check if the device is not a PC
+				// No alert, just return
+				return;
+			}
+		}
+		token = (
+			await Notifications.getExpoPushTokenAsync({
+				projectId: "fe578bec-6565-4590-b832-edb66bbb355f",
+			})
+		).data;
+	} else {
+		if (Device.brand !== "Apple" && Device.brand !== "Microsoft") { // Check if the device is not a PC
+			// No alert, just return
+			return;
+		}
+	}
+
+	return token;
+}
+
 export default function UserMainScreen() {
 	const { signOut } = useSession();
 	const [[isLoading, session], setSession] = useStorageState('session');
+	// Add a state variable for the notification toggle
+	const [expoPushToken, setExpoPushToken] = useState("");
+	const [isToggleSwitchOn, setIsToggleSwitchOn] = useState(false);
+	useEffect(() => {
+		registerForPushNotificationsAsync()
+			.then((token) => setExpoPushToken(token))
+			.catch((err) => console.log(err));
+	}, []);
+
+
+	const sendNotification = useCallback(async () => {
+		const message = {
+			to: expoPushToken,
+			sound: "default",
+			title: "Nhắc nhở  bài tập",
+			body: "Bài tập hàng ngày của bạn đã sẵn sàng. Hãy bắt đầu ngay!",
+		};
+
+		await fetch("https://exp.host/--/api/v2/push/send", {
+			method: "POST",
+			headers: {
+				host: "exp.host",
+				accept: "application/json",
+				"accept-encoding": "gzip, deflate",
+				"content-type": "application/json",
+			},
+			body: JSON.stringify(message),
+		});
+	}, [expoPushToken]);
+
+	useEffect(() => {
+		let intervalId;
+
+		if (isToggleSwitchOn) {
+			intervalId = setInterval(() => {
+				sendNotification();
+			}, 10800000); // 3 hours in milliseconds
+		}
+
+		return () => {
+			if (intervalId) {
+				clearInterval(intervalId);
+			}
+		};
+	}, [isToggleSwitchOn, sendNotification]);
+
+	//
 	const handlePress = (id: string) => {
 		switch (id) {
 			case 'log-out':
@@ -110,16 +221,27 @@ export default function UserMainScreen() {
 			case 'profile':
 				router.push('/profile');
 				break;
+			case 'notification':
+				setIsToggleSwitchOn(!isToggleSwitchOn);
+				break;
 			default:
 				console.log('default');
 				break;
 		}
 	};
 	const renderItem = ({ item }: { item: ItemData }) => {
-		return <Item item={item} onPress={() => handlePress(item.id)} />;
+		return (
+			<Item
+				item={item}
+				onPress={() => handlePress(item.id)}
+				isToggleSwitchOn={isToggleSwitchOn}
+				setIsToggleSwitchOn={setIsToggleSwitchOn}
+			/>
+		);
 	};
 	return (
 		<ThemedView style={styles.container}>
+
 			<View>
 				<Text style={styles.title}>Tổng quan</Text>
 				<FlatList
