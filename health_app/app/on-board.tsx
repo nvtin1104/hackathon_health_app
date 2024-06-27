0507521e89cac3b6351b0095fa96d8f352a9e600
ccxtncntus <ccxtncn00@gmail.com>
2024-06-27 15:06:14 +0700

    merge end

--- health_app/app/on-board.tsx
+++ health_app/app/on-board.tsx
@@ -6,76 +6,82 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { colorTheme } from '@/utils/colors';
 import { router } from 'expo-router';
 interface OnBoardingScreenProps {
-	setisFirst: React.Dispatch<React.SetStateAction<boolean>>;
+  setisFirst: React.Dispatch<React.SetStateAction<boolean>>;
 }
 interface OnboardingItem {
-	url: string;
-	title: string;
-	subtitle: string;
+  url: number;
+  title: string;
+  subtitle: string;
 }
 const OnBoardingScreen = () => {
-	const handleSkip = async () => {
-		await AsyncStorage.setItem('firstLogin', 'true');
-		router.push('/sign-in');
-	};
-	const DoneButtonComponent = () => {
-		return (
-			<TouchableOpacity onPress={handleSkip} style={{ marginRight: 20 }}>
-				<View>
-					<Text style={{ fontSize: 16 }}>Khám phá ngay</Text>
-				</View>
-			</TouchableOpacity>
-		);
-	};
+  const handleSkip = async () => {
+    await AsyncStorage.setItem('firstLogin', 'true');
+    router.push('/sign-in');
+  };
+  const DoneButtonComponent = () => {
+    return (
+      <TouchableOpacity
+        onPress={handleSkip}
+        style={{ marginRight: 20, backgroundColor: 'red' }}
+      >
+        <View>
+          <Text style={{ fontSize: 16, color: 'red' }}>Khám phá ngays</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  };
 
-	const data: OnboardingItem[] = [
-		{
-			url: 'https://i.pinimg.com/564x/24/c7/06/24c7061d68e52c77b381f86ef32873a6.jpg',
-			title: 'Onboarding',
-			subtitle: 'Done with React Native Onboarding Swiper',
-		},
-		{
-			url: 'https://i.pinimg.com/564x/22/45/7c/22457c17f09fc866d3c27fd9a999e350.jpg',
-			title: 'Onboarding1',
-			subtitle: 'Done with React Native Onboarding Swiper',
-		},
-		{
-			url: 'https://i.pinimg.com/564x/e4/27/44/e42744255952f7af0a9e3b72c3f89da9.jpg',
-			title: 'Onboarding2',
-			subtitle: 'Done with React Native Onboarding Swiper',
-		},
-	];
+  const data: OnboardingItem[] = [
+    {
+      url: require('../assets/images/kl1.png'),
+      title: 'Theo Dõi Sức Mạnh',
+      subtitle:
+        'Ghi lại các bài tập, theo dõi tiến độ và tối ưu hóa hiệu suất tập luyện của bạn',
+    },
+    {
+      url: require('../assets/images/kl2.png'),
+      title: 'Chinh Phục Mọi Cung Đường',
+      subtitle:
+        'Đo lường khoảng cách và calo đã đốt cháy trong mỗi buổi chạy của bạn',
+    },
+    {
+      url: require('../assets/images/kl3.png'),
+      title: 'Cân Bằng Cuộc Sống',
+      subtitle:
+        'Lên lịch các buổi tập yoga, theo dõi thời gian thực hành và cải thiện sự linh hoạt của bạn',
+    },
+  ];
 
-	return (
-		<View style={styles.home}>
-			<Onboarding
-				onSkip={() => handleSkip()}
-				skipLabel={'Bỏ qua'}
-				nextLabel={'Xem tiếp'}
-				onDone={handleSkip}
-				DoneButtonComponent={DoneButtonComponent}
-				bottomBarHeight={60}
-				// bottomBarColor={colorTheme.background.secondary}
-				pages={data.map((item) => ({
-					backgroundColor: colorTheme.background.secondary,
-					image: <Image source={{ uri: item.url }} style={styles.img} />,
-					title: item.title,
-					subtitle: item.subtitle,
-				}))}
-			/>
-		</View>
-	);
+  return (
+    <View style={styles.home}>
+      <Onboarding
+        onSkip={() => handleSkip()}
+        skipLabel={'Bỏ qua'}
+        nextLabel={'Xem tiếp'}
+        onDone={handleSkip}
+        DoneButtonComponent={DoneButtonComponent}
+        bottomBarHeight={60}
+        // bottomBarColor={colorTheme.background.secondary}
+        pages={data.map((item) => ({
+          backgroundColor: colorTheme.background.secondary,
+          image: <Image source={item.url} style={styles.img} />,
+          title: item.title,
+          subtitle: item.subtitle,
+        }))}
+      />
+    </View>
+  );
 };
 
 export default OnBoardingScreen;
 
 const styles = StyleSheet.create({
-	home: {
-		height: Dimensions.get('window').height,
-		backgroundColor: colorTheme.white,
-	},
-	img: {
-		width: Dimensions.get('window').width,
-		height: Dimensions.get('window').height * 0.5,
-	},
+  home: {
+    height: Dimensions.get('window').height,
+    backgroundColor: colorTheme.white,
+  },
+  img: {
+    width: Dimensions.get('window').width * 0.8,
+    height: Dimensions.get('window').height * 0.4,
+  },
 });
--- health_app/app/register.tsx
+++ health_app/app/register.tsx
@@ -1,14 +1,14 @@
 import {
-	SafeAreaView,
-	StyleSheet,
-	TextInput,
-	View,
-	TouchableOpacity,
-	Text,
-	Button,
-	Alert,
-	ToastAndroid,
-	Image,
+  SafeAreaView,
+  StyleSheet,
+  TextInput,
+  View,
+  TouchableOpacity,
+  Text,
+  Button,
+  Alert,
+  ToastAndroid,
+  Image,
 } from 'react-native';
 import React, { useEffect, useState } from 'react';
 import { colorTheme } from '@/utils/colors';
@@ -21,227 +21,279 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import Entypo from '@expo/vector-icons/Entypo';
 import { Link } from 'expo-router';
 export default function RegisterScreen() {
-	const { signIn } = useSession();
-	const [password, onChangePassword] = useState('');
-	const [email, onChangeText] = useState('');
-	const [secureTextEntry, setSecureTextEntry] = useState(true);
+  const { signIn } = useSession();
+  const [password, onChangePassword] = useState('');
+  const [passwordConfirm, onChangePasswordConfirm] = useState('');
+  const [email, onChangeText] = useState('');
+  const [secureTextEntry, setSecureTextEntry] = useState(true);
 
-	const handleLogin = () => {
-		const check = validateForm();
-		if (!check) {
-			return;
-		}
-		fetch(`${apiUrl}/users/register`, {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify({
-				email,
-				password,
-			}),
-		})
-			.then((response) => {
-				if (!response.ok) {
-					throw new Error('Network response was not ok');
-				}
-				return response.text(); // Get the raw response text
-			})
-			.then((text) => {
-				try {
-					const data = JSON.parse(text); // Attempt to parse the response as JSON
-					if (data.error) {
-						Alert.alert('Error', data.error);
-					} else {
-						console.log(data);
+  const handleLogin = () => {
+    const check = validateForm();
+    if (!check) {
+      return;
+    }
+    fetch(`${apiUrl}/users/register`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.text(); // Get the raw response text
+      })
+      .then((text) => {
+        try {
+          const data = JSON.parse(text); // Attempt to parse the response as JSON
+          if (data.error) {
+            Alert.alert('Error', data.error);
+          } else {
+            console.log(data);
 
-						if (data.success == true) {
-							showToast(data.message);
-							router.replace('/sign-in');
-						} else {
-							showToast(data.message);
-						}
-					}
-				} catch (error) {
-					Alert.alert('Error', 'Failed to parse server response');
-				}
-			})
-			.catch((error) => {
-				Alert.alert('Error', error.message);
-			});
-	};
+            if (data.success == true) {
+              showToast(data.message);
+              router.replace('/sign-in');
+            } else {
+              showToast(data.message);
+            }
+          }
+        } catch (error) {
+          Alert.alert('Error', 'Failed to parse server response');
+        }
+      })
+      .catch((error) => {
+        Alert.alert('Error', error.message);
+      });
+  };
 
-	type Errors = {
-		email?: string;
-		password?: string;
-	};
-	const [errors, setErrors] = useState<Errors>({});
+  type Errors = {
+    email?: string;
+    password?: string;
+    passwordConfirm?: string;
+  };
+  const [errors, setErrors] = useState<Errors>({});
 
-	const validateForm = () => {
-		let errors: Errors = {};
-		// Validate email field
-		if (!email) {
-			errors.email = 'Email là bắt buộc.';
-		} else if (!/\S+@\S+\.\S+/.test(email)) {
-			errors.email = 'Email không hợp lệ.';
-		}
+  const validateForm = () => {
+    let errors: Errors = {};
+    // Validate email field
+    if (!email) {
+      errors.email = 'Email là bắt buộc.';
+    } else if (!/\S+@\S+\.\S+/.test(email)) {
+      errors.email = 'Email không hợp lệ.';
+    }
 
-		// Validate password field
-		if (!password) {
-			errors.password = 'Mật khẩu là bắt buộc.';
-		} else if (password.length < 3) {
-			errors.password = 'Mật khẩu phải có ít nhất 3 ký tự.';
-		}
-		if (Object.keys(errors).length != 0) {
-			setErrors(errors);
-			return false;
-		}
-		setErrors({});
-		return true;
-	};
+    // Validate password field
+    if (!password) {
+      errors.password = 'Mật khẩu là bắt buộc.';
+    } else if (password.length < 3) {
+      errors.password = 'Mật khẩu phải có ít nhất 3 ký tự.';
+    }
+    if (!passwordConfirm) {
+      errors.passwordConfirm = 'Hãy nhập lại mật khẩu';
+    } else if (passwordConfirm.length < 3) {
+      errors.password = 'Mật khẩu phải có ít nhất 3 ký tự.';
+    } else if (passwordConfirm !== password) {
+      errors.passwordConfirm = 'Nhập lại mật khẩu không đúng';
+    }
+    if (Object.keys(errors).length != 0) {
+      setErrors(errors);
+      return false;
+    }
+    setErrors({});
+    return true;
+  };
 
-	return (
-		<View
-			style={{
-				flex: 1,
-				justifyContent: 'center',
-				alignItems: 'center',
-				backgroundColor: colorTheme.background.secondary,
-			}}
-		>
-			<Image
-				style={styles.image}
-				source={{
-					uri: 'https://i.pinimg.com/736x/9f/93/ae/9f93ae8f39417cd575e735bf5f1b1505.jpg',
-				}}
-			/>
-			<Text style={{ fontFamily: 'LeagueLight', fontSize: 32, margin: 8 }}>
-				Healthy Care
-			</Text>
-			<Text
-				style={{
-					fontFamily: 'LeagueMedium',
-					marginBottom: 20,
-					fontSize: 16,
-				}}
-			>
-				Voi Tây nguyên
-			</Text>
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: colorTheme.background.secondary,
+      }}
+    >
+      <Image
+        style={styles.image}
+        source={require('../assets/images/logod.png')}
+      />
+      <Text style={{ fontFamily: 'LeagueLight', fontSize: 32, margin: 8 }}>
+        Healthy Care
+      </Text>
+      <Text
+        style={{
+          fontFamily: 'LeagueMedium',
+          marginBottom: 20,
+          fontSize: 16,
+        }}
+      >
+        Voi Tây nguyên
+      </Text>
 
-			<SafeAreaView>
-				<Text style={{ fontFamily: 'LeagueLight', fontSize: 16 }}>
-					Tên đăng nhập
-				</Text>
-				<TextInput
-					style={styles.inputDone}
-					onChangeText={onChangeText}
-					placeholder="Nhập tên đăng nhập"
-					value={email}
-				/>
-				{errors.email && <Text style={styles.error}>{errors.email}</Text>}
-				<Text
-					style={{ fontFamily: 'LeagueLight', fontSize: 16, marginTop: 12 }}
-				>
-					Mật khẩu
-				</Text>
-				<View style={styles.passwordContainer}>
-					<TextInput
-						style={styles.passwordInput}
-						onChangeText={onChangePassword}
-						value={password}
-						placeholder="Mật khẩu"
-						secureTextEntry={secureTextEntry}
-					/>
-					<TouchableOpacity
-						style={styles.toggleButton}
-						onPress={() => setSecureTextEntry(!secureTextEntry)}
-					>
-						<Text>
-							{secureTextEntry ? (
-								<Entypo name="eye" size={16} color="black" />
-							) : (
-								<Entypo name="eye-with-line" size={16} color="black" />
-							)}
-						</Text>
-					</TouchableOpacity>
-				</View>
-				{errors.password && <Text style={styles.error}>{errors.password}</Text>}
-				<TouchableOpacity
-					style={[
-						styles.button,
-						{ opacity: email == '' || password == '' ? 0.5 : 1 },
-					]}
-					disabled={email == '' || password == ''}
-					onPress={handleLogin}
-				>
-					<Text style={styles.textButton}>Đăng ký</Text>
-				</TouchableOpacity>
-			</SafeAreaView>
-			<Text style={{ fontFamily: 'LeagueLight', marginTop: 20 }}>
-				Đã có tài khoản? <Link href="/sign-in">Đăng nhập</Link>
-			</Text>
-		</View>
-	);
+      <SafeAreaView>
+        <Text style={{ fontFamily: 'LeagueLight', fontSize: 16 }}>
+          Tên đăng nhập
+        </Text>
+        <TextInput
+          style={styles.inputDone}
+          onChangeText={onChangeText}
+          placeholder="Nhập tên đăng nhập"
+          value={email}
+        />
+        {errors.email && <Text style={styles.error}>{errors.email}</Text>}
+        <Text
+          style={{ fontFamily: 'LeagueLight', fontSize: 16, marginTop: 12 }}
+        >
+          Mật khẩu
+        </Text>
+        <View style={styles.passwordContainer}>
+          <TextInput
+            style={styles.passwordInput}
+            onChangeText={onChangePassword}
+            value={password}
+            placeholder="Mật khẩu"
+            secureTextEntry={secureTextEntry}
+          />
+          <TouchableOpacity
+            style={styles.toggleButton}
+            onPress={() => setSecureTextEntry(!secureTextEntry)}
+          >
+            <Text>
+              {secureTextEntry ? (
+                <Entypo name="eye" size={16} color="black" />
+              ) : (
+                <Entypo name="eye-with-line" size={16} color="black" />
+              )}
+            </Text>
+          </TouchableOpacity>
+        </View>
+        {errors.password && <Text style={styles.error}>{errors.password}</Text>}
+        {/* ----------------------- */}
+        <Text
+          style={{ fontFamily: 'LeagueLight', fontSize: 16, marginTop: 12 }}
+        >
+          Nhập lại mật khẩu
+        </Text>
+        <View style={styles.passwordContainer}>
+          <TextInput
+            style={styles.passwordInput}
+            onChangeText={onChangePasswordConfirm}
+            value={passwordConfirm}
+            placeholder="Nhập lại mật khẩu"
+            secureTextEntry={secureTextEntry}
+          />
+          <TouchableOpacity
+            style={styles.toggleButton}
+            onPress={() => setSecureTextEntry(!secureTextEntry)}
+          >
+            <Text>
+              {secureTextEntry ? (
+                <Entypo name="eye" size={16} color="black" />
+              ) : (
+                <Entypo name="eye-with-line" size={16} color="black" />
+              )}
+            </Text>
+          </TouchableOpacity>
+        </View>
+        {errors.passwordConfirm && (
+          <Text style={styles.error}>{errors.passwordConfirm}</Text>
+        )}
+        {/* ----------------------- */}
+
+        <TouchableOpacity
+          style={[
+            styles.button,
+            {
+              opacity:
+                email == '' ||
+                password == '' ||
+                passwordConfirm == '' ||
+                passwordConfirm !== password
+                  ? 0.5
+                  : 1,
+            },
+          ]}
+          disabled={
+            email == '' ||
+            password == '' ||
+            passwordConfirm == '' ||
+            passwordConfirm !== password
+          }
+          onPress={handleLogin}
+        >
+          <Text style={styles.textButton}>Đăng ký</Text>
+        </TouchableOpacity>
+      </SafeAreaView>
+      <Text style={{ fontFamily: 'LeagueLight', marginTop: 20 }}>
+        Đã có tài khoản? <Link href="/sign-in">Đăng nhập</Link>
+      </Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-	inputDone: {
-		fontFamily: 'LeagueLight',
-		marginTop: 10,
-		fontSize: 16,
-		marginBottom: 10,
-		paddingHorizontal: 12,
-		paddingVertical: 12,
-		elevation: 0,
-		borderRadius: 2,
-		width: 300,
-		borderColor: colorTheme.background.primary,
-		backgroundColor: colorTheme.white,
-	},
+  inputDone: {
+    fontFamily: 'LeagueLight',
+    marginTop: 10,
+    fontSize: 16,
+    marginBottom: 10,
+    paddingHorizontal: 12,
+    paddingVertical: 12,
+    elevation: 0,
+    borderRadius: 2,
+    width: 300,
+    borderColor: colorTheme.background.primary,
+    backgroundColor: colorTheme.white,
+  },
 
-	button: {
-		backgroundColor: colorTheme.background.primary,
-		padding: 12,
-		borderRadius: 36,
-		marginTop: 20,
-		width: 300,
-		textAlign: 'center',
-		alignItems: 'center',
-	},
-	textButton: {
-		color: colorTheme.white,
-	},
-	passwordContainer: {
-		flexDirection: 'row',
-		alignItems: 'center',
-		marginBottom: 12,
-		marginTop: 12,
-		borderRadius: 2,
-		backgroundColor: colorTheme.white,
-	},
-	passwordInput: {
-		flex: 1,
-		fontFamily: 'LeagueLight',
-		fontSize: 16,
-		paddingHorizontal: 12,
-		paddingVertical: 12,
-		elevation: 0,
-		borderRadius: 2,
-		borderColor: colorTheme.background.primary,
-		backgroundColor: colorTheme.white,
-	},
-	error: {
-		color: colorTheme.lightRed,
-		fontFamily: 'LeagueLight',
-		marginBottom: 10,
-		fontSize: 16,
-	},
-	toggleButton: {
-		padding: 10,
-	},
-	image: {
-		width: 80,
-		height: 80,
-		objectFit: 'cover',
-	},
+  button: {
+    backgroundColor: colorTheme.background.primary,
+    padding: 12,
+    borderRadius: 36,
+    marginTop: 20,
+    width: 300,
+    textAlign: 'center',
+    alignItems: 'center',
+  },
+  textButton: {
+    color: colorTheme.white,
+  },
+  passwordContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 12,
+    marginTop: 12,
+    borderRadius: 2,
+    backgroundColor: colorTheme.white,
+  },
+  passwordInput: {
+    flex: 1,
+    fontFamily: 'LeagueLight',
+    fontSize: 16,
+    paddingHorizontal: 12,
+    paddingVertical: 12,
+    elevation: 0,
+    borderRadius: 2,
+    borderColor: colorTheme.background.primary,
+    backgroundColor: colorTheme.white,
+  },
+  error: {
+    color: colorTheme.lightRed,
+    fontFamily: 'LeagueLight',
+    marginBottom: 10,
+    fontSize: 16,
+  },
+  toggleButton: {
+    padding: 10,
+  },
+  image: {
+    width: 88,
+    height: 88,
+    objectFit: 'cover',
+  },
 });
